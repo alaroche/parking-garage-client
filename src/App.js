e1690c45c59d93e29fbb5b9f84dc713fa0715db8
Aaron <alaroche22@gmail.com>
2021-06-22 21:51:45 -0400

    unsoil

--- src/App.js
+++ src/App.js
@@ -1,62 +1,66 @@
 // @flow
 
 import * as React from "react";
+import logo from './logo.svg';
+import './App.css';
 
 import {
   Page,
   Grid,
   Card,
   colors,
+  Site,
+  SiteHeader,
 } from "tabler-react";
 
 import C3Chart from "react-c3js";
 
-import SiteWrapper from "./SiteWrapper.react";
-
 function App() {
   return (
-    <SiteWrapper>
-      <Page.Content title="Dashboard">
-            <Grid.Row>
-              <Grid.Col sm={6}>
-                <Card>
-                  <Card.Header>
-                    <Card.Title>Chart title</Card.Title>
-                  </Card.Header>
-                  <Card.Body>
-                    <C3Chart
-                      style={{ height: "12rem" }}
-                      data={{
-                        columns: [
-                          // each columns data
-                          ["data1", 63],
-                          ["data2", 37],
-                        ],
-                        type: "donut", // default type of chart
-                        colors: {
-                          data1: colors["green"],
-                          data2: colors["green-light"],
-                        },
-                        names: {
-                          // name of each serie
-                          data1: "Maximum",
-                          data2: "Minimum",
-                        },
-                      }}
-                      legend={{
-                        show: false, //hide legend
-                      }}
-                      padding={{
-                        bottom: 0,
-                        top: 0,
-                      }}
-                    />
-                  </Card.Body>
-                </Card>
-              </Grid.Col>
-        </Grid.Row>
-      </Page.Content>
-    </SiteWrapper>
+    <Site>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Welcome to Parking Garage
+        </p>
+        </header>
+        <Card>
+          <Card.Header>
+            <Card.Title>Chart title</Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <C3Chart
+              style={{ height: "12rem" }}
+              data={{
+                columns: [
+                  // each columns data
+                  ["data1", 63],
+                  ["data2", 37],
+                ],
+                type: "donut", // default type of chart
+                colors: {
+                  data1: colors["blue-dark"],
+                  data2: colors["blue-light"],
+                },
+                names: {
+                  // name of each serie
+                  data1: "Maximum",
+                  data2: "Minimum",
+                },
+              }}
+              legend={{
+                show: false, //hide legend
+              }}
+              padding={{
+                bottom: 0,
+                top: 0,
+              }}
+            />
+          </Card.Body>
+        </Card>
+      </div>
+    </Site>
   );
 }
 
