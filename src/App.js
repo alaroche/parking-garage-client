d8a90b425f97a1646080ee3f493a1ce20b22ce87
Aaron <alaroche22@gmail.com>
2021-07-03 19:56:40 -0400

    remove unused styles

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="header">
         <img src={logo} className="header__logo" alt="logo" />
         <div className="header__title">Available Parking</div>
