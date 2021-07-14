13c9c54cd3589cfde53b1a4778651bfe5eb1a13a
Aaron <alaroche22@gmail.com>
2021-07-14 10:41:03 -0400

    app into component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import MainPieChart from "./components/MainPieChart";
 import MinorPieChart from './components/MinorPieChart';
 import logo from './images/logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <header className="header">
-        <img src={logo} className="header__logo" alt="logo" />
-        <div className="header__title">Available Parking</div>
-      </header>
-      <div className="main-pie-chart">
-        <MainPieChart />
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <header className="header">
+          <img src={logo} className="header__logo" alt="logo" />
+          <div className="header__title">Available Parking</div>
+        </header>
+        <div className="main-pie-chart">
+          <MainPieChart />
+        </div>
+        <hr />
+        <div className="minor-pie-charts">
+          <MinorPieChart />
+          <MinorPieChart />
+          <MinorPieChart />
+          <MinorPieChart />
+          <MinorPieChart />
+        </div>
       </div>
-      <hr />
-      <div className="minor-pie-charts">
-        <MinorPieChart />
-        <MinorPieChart />
-        <MinorPieChart />
-        <MinorPieChart />
-        <MinorPieChart />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
